07a05c15a1e80114bbbf0df139a07ecec8b414ac
lngwashi <lengwash@gmail.com>
2023-04-03 21:51:03 -0400

    seventh mesaage

---
+++ output.tf
@@ -0,0 +1,10 @@
+output "instance_publicip" {
+  description = "EC2 Instance Public IP"
+  value       = aws_instance.myec2vm.public_ip
+}
+
+
+output "instance_publicdns" {
+  description = "EC2 Instance Public DNS"
+  value       = aws_instance.myec2vm.public_dns
+}
