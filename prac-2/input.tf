3b95cffa390ec62a37bd07a544978c2f4fddccff
lngwashi <lengwash@gmail.com>
2023-04-04 17:11:48 -0400

    ninth commit message

---
+++ prac-2/input.tf
@@ -0,0 +1,61 @@
+# AWS Region 
+
+variable "aws_region" {
+  description = "Region in which AWS Resources to be created"
+  type        = string
+  default     = "us-east-1"
+}
+
+# AWS EC2 Instance Type 
+
+variable "instance_type" {
+  description = "EC2 Instnace Type"
+  type        = string
+  default     = "t3.micro"
+}
+
+# AWS EC2 Instance Key Pair 
+
+variable "instance_keypair" {
+  description = "AWS EC2 Key Pair that need to be associated with EC2 Instance"
+  type        = string
+  default     = "Automationkey"
+}
+
+# AWS EC2 Instance Type - List 
+
+variable "instance_type_list" {
+  description = "EC2 Instance Type"
+  type        = list(string)
+  default     = ["t2.micro", "t2.medium", "t3.large"]
+}
+
+# AWS EC2 Instance Type - Map 
+
+variable "instance_type_map" {
+  description = "EC2 Instance Type"
+  type        = map(string)
+  default = {
+    "dev"  = "t3.micro"
+    "qa"   = "t3.small"
+    "prod" = "t3.large"
+  }
+}
+
+# make sure to put all the variables above in a .tf file and reference it when creating a resource
+
+
+# EC2 Instance 
+resource "aws_instance" "myec2" {
+  ami = data.aws_ami.amzlinux2.id
+  #instance_type = var.instance_type 
+  instance_type = var.instance_type_list[0] # For List--→i.e t2.micro for index 0 
+  #instance_type = var.instance_type_map["dev"] # For Map 
+  user_data              = file("${path.module}/app1-install.sh")
+  key_name               = var.instance_keypair
+  vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
+  count                  = 2 # how many instances to create and each named with a count index i.e prod-0, prod-1 
+  tags = {
+    "Name" = "Prod-${count.index}"
+  }
+}
