442a07d31c051458c73589b9aa21f5ab99b6a09b
lngwashi <lengwash@gmail.com>
2023-04-03 21:08:34 -0400

    Second commit message

--- prac-1/ec2-instance.tf
+++ prac-1/ec2-instance.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = "~> 1.0" #1.1.4/5/6/7   1.2/3/4/5 1.1.4/5/6/7
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+}
+
 provider "aws" {
   profile = "default" # AWS Credentials Profile configured on your local desktop terminal $HOME/.aws/credentials
   region  = "us-east-1"
